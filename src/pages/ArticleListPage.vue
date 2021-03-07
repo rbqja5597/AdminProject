fa2e101d5ac27f801c0782a3048cfeb352e5c6df
rbqja5597 <rbqja5597@naver.com>
2021-03-08 06:14:34 +0900

    4

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -1,23 +1,21 @@
 <template>
-  <section class="title-bar">
-    <h1 class="container mx-auto">
-      게시물 리스트 페이지
-    </h1>
-
-  </section>
+  <TitleBar title="게시물 리스트" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import TitleBar from '../components/TitleBar.vue';
 
 export default defineComponent({
-  name: 'ArticleListPage',  
+  name: 'ArticleListPage',
+  components: {
+    TitleBar
+  }
+
 })
 
 </script>
 
 <style scoped>
-  h1 {
-    color:red;
-  }
+
 </style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,21 +1,20 @@
 <template>
- <section class="title-bar">
-    <h1 class="container mx-auto">
-      홈 메인 페이지
-    </h1>
-
-  </section>
+  <TitleBar title="홈 메인" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import TitleBar from '../components/TitleBar.vue';
 
 export default defineComponent({
   name: 'MainPage',  
+  components: {
+    TitleBar
+  }
 })
 
 </script>
 
-<style>
+<style scoped>
 
 </style>
