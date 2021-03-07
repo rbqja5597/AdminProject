4985b9225870f3d66f6fe1b1e511da2e9a4006e4
rbqja5597 <rbqja5597@naver.com>
2021-03-08 06:20:24 +0900

    4

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -4,13 +4,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import TitleBar from '../components/TitleBar.vue';
+
 
 export default defineComponent({
   name: 'MainPage',  
-  components: {
-    TitleBar
-  }
 })
 
 </script>
