bee1dac7156d0d0868aaae7b5742a13ed730cbd6
rbqja5597 <rbqja5597@naver.com>
2021-03-08 05:30:49 +0900

    1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  test
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  
+})
+
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
